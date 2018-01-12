b227a27826aa021c02fd3ccf1259f61be8a229ca
Maximilian Meyer <maximilian.meyer@br.de>
2018-01-12 12:16:22 +0100

    Updates

--- Webclient/src/app/app-routing.module.ts
+++
@@ -1,4 +0,0 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';     // Add this
-import { AboutComponent } from './about/about.component';  // Add this
