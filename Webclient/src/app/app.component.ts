b6d03351314abf7838cfb230b72fb5f755fad872
Maximilian Meyer <maximilian.meyer@br.de>
2018-01-29 17:39:04 +0100

    cleanup

--- Webclient/src/app/app.component.ts
+++ Webclient/src/app/app.component.ts
@@ -16,28 +16,19 @@ export class AppComponent implements OnInit {
   myData: Array<any>;
   uniqueId: string;
   path: string;
-  // @Input() currentPath: string;
 
   constructor(private http: Http, private route: ActivatedRoute, private router: Router) {
     // this.route.params.subscribe(params => (this.path = `/${params.id}`));
-    // console.log('@constructor path: ' + this.path);
-    // this.http
-    //   .get('http://api.icndb.com/jokes/random/20')
-    //   .map(response => response.json())
-    //   .subscribe(res => (this.myData = res));
   }
   ngOnInit() {
+
+    //HACK: extract unique ID from route & add to navigation
     let getLocation = function(href) {
       let l = document.createElement("a");
       l.href = href;
       return l;
-  };
+    };
   let l = getLocation(window.location.href);
-  // console.log(l.hostname)
-  // console.log(l.pathname)
   this.uniqueId = l.pathname.match(/\/([^/]*)$/)[1];
-  console.log(this.uniqueId);
-    // console.log(this.route.snapshot.queryParamMap);
-    // console.log('My route: ', this.router.url);
   }
 }
